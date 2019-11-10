3dcd38a44b651c70965ab786c96506b9a0c00765
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-11-10 23:55:16 +0530

    Changes in UserView
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- mysite/feedback/views.py
+++ mysite/feedback/views.py
@@ -6,11 +6,25 @@ from rest_framework.response import Response
 
 
 class UserView(viewsets.ModelViewSet):
-	queryset = User.objects.all()
 	serializer_class = UserSerializer
+	queryset = User.objects.all()
+
+	def create(self, request):
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		email = request.POST.get('email')
+		user = User.objects.get_or_create(username, email, password)
+		if created:
+			return response({'message' : 'User created successfully!!'}, status=200)
+		return response({'message' : 'Sorry! The user already exsists'}, status=200)
 
 	def list(self, request):
 		serializer = UserSerializer(self.get_queryset(), many=True)
+		return Response(serializer.data)
+
+	def retrieve(self, request, pk=None):
+		user = get_object_or_404(self.get_queryset(), pk=pk)
+		serializer = UserSerializer(user)
 		return Response(serializer.data)
 
 	def destroy(self, request, pk=None):
